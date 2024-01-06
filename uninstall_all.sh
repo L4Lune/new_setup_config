0b347258687d8209eb5d7af126ce3ff69dc9d504
Jack Roudebush <roudebushjack@gmail.com>
2024-01-06 11:08:28 -0500

    add defaults to command

--- uninstall_all.sh
+++ uninstall_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 /bin/zsh uninstall_oh_my_zsh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
-delete com.apple.dock; killall Dock
+defaults delete com.apple.dock; killall Dock
 rm -rf ~/repos
