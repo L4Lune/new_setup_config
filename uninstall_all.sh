8fbf8a734c09ed8e64d8e9318566c4891a45bc87
Jack Roudebush <roudebushjack@gmail.com>
2024-01-06 14:29:40 -0500

    reorder uninstall commands

--- uninstall_all.sh
+++ uninstall_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 /bin/zsh uninstall_oh_my_zsh
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
 brew remove --force $(brew list --formula)
 brew remove --cask --force $(brew list)
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
 defaults delete com.apple.dock; killall Dock
 rm -rf ~/repos
