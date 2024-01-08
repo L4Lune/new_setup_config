bcfb8e150b0a43d839bed06cd019b86364c1ab80
Jack Roudebush <roudebushjack@gmail.com>
2024-01-08 00:24:44 -0500

    add this repos directory to remove rather than the entire repos

--- uninstall_all.sh
+++ uninstall_all.sh
@@ -4,4 +4,4 @@ brew remove --force $(brew list --formula)
 brew remove --cask --force $(brew list)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
 defaults delete com.apple.dock; killall Dock
-rm -rf ~/repos
+rm -rf ~/repos/new_setup_config
