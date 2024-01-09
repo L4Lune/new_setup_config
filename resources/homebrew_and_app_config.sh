168e4bfc9a067232acaa50abb3329e97b05f6e4b
Jack Roudebush <roudebushjack@gmail.com>
2024-01-08 20:56:29 -0500

    ran through shellcheck

--- resources/homebrew_and_app_config.sh
+++ resources/homebrew_and_app_config.sh
@@ -4,25 +4,26 @@
 
 # All HomeBrew Options
 configure_homebrew_and_apps () {
-  get_homebrew
-  add_homebrew_to_path
-  get_apps_from_brewfile
+	get_homebrew
+	add_homebrew_to_path
+	get_apps_from_brewfile
 }
 
 # Install HomeBrew if not already installed
 get_homebrew () {
 	echo -e "Checking if HomeBrew is installed..."
-	if test ! $(which brew); then
+	if test ! "$(which brew)"; then
 		echo -e "Installing HomeBrew..."
 		/bin/bash -c \
 		"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	fi
 	brew update
 }
 
 # Add HomeBrew to PATH
 add_homebrew_to_path () {
 	echo -e "Adding HomeBrew to PATH..."
-	(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/$USER/.zprofile
+	(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/"$USER"/.zprofile
 	eval "$(/opt/homebrew/bin/brew shellenv)"
 }
       
@@ -34,8 +35,8 @@ get_apps_from_brewfile () {
 
 uninstall_apps_from_brewfile () {
 	echo "Uninstalling all applications managed by HomeBrew..."
-	brew remove --force $(brew list --formula)
-	brew remove --cask --force $(brew list)
+	brew remove --force "$(brew list --formula)"
+	brew remove --cask --force "$(brew list)"
 }
 
 update_all_homebrew_apps () {
@@ -51,7 +52,7 @@ uninstall_homebrew () {
 # Install VS Code Extensions
 install_vscode_ext () {
 	echo -e "Installing VS Code extenstions..."
-	while read extension; do 
-		code --install-extension $extension
+	while read -r extension; do 
+		code --install-extension "$extension"
 	done < ./vscode_ext
 }
