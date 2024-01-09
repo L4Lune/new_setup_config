754f1dff838ad1b1e8fa6ec2ed0d3abb32f2b18d
Jack Roudebush <roudebushjack@gmail.com>
2024-01-08 21:24:24 -0500

    add more spacers

--- resources/dock_config.sh
+++ resources/dock_config.sh
@@ -24,7 +24,7 @@ dock_hiding () {
 # Add 6 small spacers to Dock
 add_dock_spacers () {
   echo -e "Adding spacers for dock organization..."
-  i=6
+  i=8
   while [ $i -gt 0 ]
   do
     defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="small-spacer-tile";}'; killall Dock
